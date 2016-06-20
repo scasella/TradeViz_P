53ccbe54f357deb15ab1a8abc94f2c6a0d256cfe
Stephen Casella <sjcasella@icloud.com>
2016-06-20 13:55:22 -0400

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -5,34 +5,40 @@ import cPickle
 exportArr = []
 
 def yahooLoad(val):
-    global exportArr
-    string = ""
-    tempArr = []
-    string = 'http://ichart.finance.yahoo.com/table.csv?s={0}'.format(val)
-
-    csv = urllib2.urlopen(string).readlines()
-    #for bar in xrange(1,min(len(csv),500)):
-    for bar in xrange(1,len(csv)):
-        close = csv[bar].split(',')[6]
-        close = float(close)
-        tempArr.append(close)
-    tempArr = tempArr[::-1]
-    exportArr.append(tempArr)
+    try:    
+        global exportArr
+        string = ""
+        tempArr = []
+        string = 'http://ichart.finance.yahoo.com/table.csv?s={0}'.format(val)
+    
+        csv = urllib2.urlopen(string).readlines()
+        #for bar in xrange(1,min(len(csv),500)):
+        for bar in xrange(1,len(csv)):
+            close = csv[bar].split(',')[6]
+            close = float(close)
+            tempArr.append(close)
+        tempArr = tempArr[::-1]
+        exportArr.append(tempArr)
+    except:
+        pass
 
 
 def loadQuote(val):
-    global exportArr
-    global interval
-    tempArr = []
-    string = ""
-    string = 'https://www.google.com/finance/getprices?q={0}&i={1}&p=200d&f=d,c'.format(val,interval)
-
-    csv = urllib2.urlopen(string).readlines()
-    for bar in xrange(7,len(csv)):
-        offset,close = csv[bar].split(',')
-        if offset[0]!='a':
-            tempArr.append(float(close))
-    exportArr.append(tempArr)
+    try:
+        global exportArr
+        global interval
+        tempArr = []
+        string = ""
+        string = 'https://www.google.com/finance/getprices?q={0}&i={1}&p=200d&f=d,c'.format(val,interval)
+    
+        csv = urllib2.urlopen(string).readlines()
+        for bar in xrange(7,len(csv)):
+            offset,close = csv[bar].split(',')
+            if offset[0]!='a':
+                tempArr.append(float(close))
+        exportArr.append(tempArr)
+    except:
+        pass
 
 
 
