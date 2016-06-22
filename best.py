0139be91c0f799a485a6156c20b9f5c3eab9ecae
sjcasella <sjcasella@icloud.com>
2016-06-22 15:08:40 -0400

    Full SPX

--- best.py
+++ best.py
@@ -9,7 +9,7 @@ import urllib2
 from multiprocessing.dummy import Pool as ThreadPool
 import math
 import cPickle
-import os 
+import os
 
 
 # In[5]:
@@ -80,7 +80,7 @@ def yahooLoad(val):
     tempArr = tempArr[::-1]
     return tempArr
 
-    
+
 def currentPat(curArr):
     global patLen
     curPat = []
@@ -95,7 +95,7 @@ def currentPat(curArr):
         curPat.append(temp)
         i += 1
     return curPat
-        
+
 
 def sortPats(array):
     finalArr = []
@@ -149,14 +149,14 @@ def plotting(matchedPat,matchedEndInd):
             futurePercent.append(tempPercent)
 
         futureAverages = []
-        
+
         for arrItemNum in range(len(futurePercent[0])):
             tempOutcomes = []
             for arr in futurePercent:
                 tempOutcomes.append(arr[arrItemNum])
             futureAverages.append(np.mean(tempOutcomes))
             stDev.append(np.std(tempOutcomes))
-        
+
         return futureAverages,stDev
 
 
@@ -187,42 +187,43 @@ def bestGo(val):
 
 # In[19]:
 
-arr = ['MMM','ABT','ABBV','ACN','ATVI','AYI']#'ADBE','AAP','AES','AET','AMG','AFL','A','GAS','APD','AKAM']
-#'ALK','AA','ALXN','ALLE','AGN','ADS','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP','AXP','AIG','AMT',
-#'AWK','AMP','ABC','AME','AMGN','APH','APC','ADI','ANTM','AON','APA','AIV','AAPL','AMAT','ADM','AJG','AIZ','T','ADSK','ADP']
-#'AN','AZO','AVGO','AVB','AVY','BHI','BLL','BAC','BCR','BAX','BBT','BDX','BBBY','BRK-B','BBY','BIIB','BLK','HRB','BA','BWA','BXP',
-#'BSX','BMY','BF-B','CHRW','CA','CVC','COG','CPB','COF','CAH','KMX','CCL','CAT','CBG','CBS','CELG','CNC','CNP','CTL','CERN','CF',
-#'SCHW','CHK','CVX','CMG','CB','CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG','CTXS','CME','CMS','COH','CTSH','CL','CPGX','CMCSA',
-#'CMA','CAG','CXO','COP','ED','STZ','GLW','COST','CCI','CSRA','CSX','CMI','CVS','DHI','DHR','DRI','DVA','DE','DLPH','DAL','XRAY',
-#'DVN','DO','DLR','DFS','DISCA','DISCK','DG','DLTR','D','DOV','DOW','DPS','DTE','DD','DUK','DNB','ETFC','EMN','ETN','EBAY','ECL',
-#'EIX','EW','EA','EMC','EMR','ENDP','ETR','EOG','EQT','EFX','EQIX','EQR','ESS','EL','ES','EXC','EXPE','EXPD','ESRX','EXR','XOM',
-#'FFIV','FB','FAST','FRT','FDX','FIS','FITB','FSLR','FE','FISV','FLIR','FLS','FLR','FMC','FTI','FL','F','BEN','FCX','FTR','GPS',
-#'GRMN','GD','GE','GGP','GIS','GM','GPC','GILD','GPN','GS','GT','GWW','HAL','HBI','HOG','HAR','HRS','HIG','HAS','HCA','HCP','HP',
-#'HSIC','HES','HPE','HOLX','HD','HON','HRL','HST','HPQ','HUM','HBAN','ITW','ILMN','IR','INTC','ICE','IBM','IP','IPG','IFF','INTU',
-#'ISRG','IVZ','IRM','JBHT','JEC','JNJ','JCI','JPM','JNPR','KSU','K','KEY','KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LLL','LH',
-#'LRCX','LM','LEG','LEN','LUK','LVLT','LLY','LNC','LLTC','LKQ','LMT','L','LOW','LYB','MTB','MAC','M','MNK','MRO','MPC','MAR','MMC',
-#'MLM','MAS','MA','MAT','MKC','MCD','MCK','MJN','MDT','MRK','MET','KORS','MCHP','MU','MSFT','MHK','TAP','MDLZ','MON','MNST','MCO','MS',
-#'MSI','MUR','MYL','NDAQ','NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NWS','NEE','NLSN','NKE','NI','NBL','JWN','NSC','NTRS',
-#'NOC','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE','ORCL','OI','PCAR','PH','PDCO','PAYX','PYPL','PNR','PBCT','PEP','PKI','PRGO','PFE',
-#'PCG','PM','PSX','PNW','PXD','PBI','PNC','RL','PPG','PPL','PX','PCLN','PFG','PG','PGR','PLD','PRU','PEG','PSA','PHM','PVH','QRVO',
-#'QCOM','PWR','DGX','RRC','RTN','O','RHT','REGN','RF','RSG','RAI','RHI','ROK','COL','ROP','ROST','RCL','R','SPGI','CRM','SCG','SLB',
-#'SNI','STX','SEE','SRE','SHW','SIG','SPG','SWKS','SLG','SJM','SNA','SO','LUV','SWN','SE','STJ','SWK','SPLS','SBUX','HOT','STT','SRCL',
-#'SYK','STI','SYMC','SYF','SYY','TROW','TGT','TEL','TE','TGNA','TDC','TSO','TXN','TXT','BK','CLX','KO','HSY','MOS','TRV','DIS','TMO',
-#'TIF','TWX','TJX','TMK','TSS','TSCO','TDG','RIG','TRIP','FOXA','FOX','TYC','TSN','USB','UDR','ULTA','UA','UNP','UAL','UNH','UPS',
-#'URI','UTX','UHS','UNM','URBN','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','VIAB','V','VNO','VMC','WMT','WBA','WM','WAT','WFC',
-#'HCN','WDC','WU','WRK','WY','WHR','WFM','WMB','WLTW','WEC','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YHOO','YUM','ZBH','ZION','ZTS']
+arr = ['MMM','ABT','ABBV','ACN','ATVI','AYI','ADBE','AAP','AES','AET','AMG','AFL','A','GAS','APD','AKAM',
+'ALK','AA','ALXN','ALLE','AGN','ADS','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP','AXP','AIG','AMT',
+'AWK','AMP','ABC','AME','AMGN','APH','APC','ADI','ANTM','AON','APA','AIV','AAPL','AMAT','ADM','AJG','AIZ','T','ADSK','ADP',
+'AN','AZO','AVGO','AVB','AVY','BHI','BLL','BAC','BCR','BAX','BBT','BDX','BBBY','BRK-B','BBY','BIIB','BLK','HRB','BA','BWA','BXP',
+'BSX','BMY','BF-B','CHRW','CA','CVC','COG','CPB','COF','CAH','KMX','CCL','CAT','CBG','CBS','CELG','CNC','CNP','CTL','CERN','CF',
+'SCHW','CHK','CVX','CMG','CB','CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG','CTXS','CME','CMS','COH','CTSH','CL','CPGX','CMCSA',
+'CMA','CAG','CXO','COP','ED','STZ','GLW','COST','CCI','CSRA','CSX','CMI','CVS','DHI','DHR','DRI','DVA','DE','DLPH','DAL','XRAY',
+'DVN','DO','DLR','DFS','DISCA','DISCK','DG','DLTR','D','DOV','DOW','DPS','DTE','DD','DUK','DNB','ETFC','EMN','ETN','EBAY','ECL',
+'EIX','EW','EA','EMC','EMR','ENDP','ETR','EOG','EQT','EFX','EQIX','EQR','ESS','EL','ES','EXC','EXPE','EXPD','ESRX','EXR','XOM',
+'FFIV','FB','FAST','FRT','FDX','FIS','FITB','FSLR','FE','FISV','FLIR','FLS','FLR','FMC','FTI','FL','F','BEN','FCX','FTR','GPS',
+'GRMN','GD','GE','GGP','GIS','GM','GPC','GILD','GPN','GS','GT','GWW','HAL','HBI','HOG','HAR','HRS','HIG','HAS','HCA','HCP','HP',
+'HSIC','HES','HPE','HOLX','HD','HON','HRL','HST','HPQ','HUM','HBAN','ITW','ILMN','IR','INTC','ICE','IBM','IP','IPG','IFF','INTU',
+'ISRG','IVZ','IRM','JBHT','JEC','JNJ','JCI','JPM','JNPR','KSU','K','KEY','KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LLL','LH',
+'LRCX','LM','LEG','LEN','LUK','LVLT','LLY','LNC','LLTC','LKQ','LMT','L','LOW','LYB','MTB','MAC','M','MNK','MRO','MPC','MAR','MMC',
+'MLM','MAS','MA','MAT','MKC','MCD','MCK','MJN','MDT','MRK','MET','KORS','MCHP','MU','MSFT','MHK','TAP','MDLZ','MON','MNST','MCO','MS',
+'MSI','MUR','MYL','NDAQ','NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NWS','NEE','NLSN','NKE','NI','NBL','JWN','NSC','NTRS',
+'NOC','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE','ORCL','OI','PCAR','PH','PDCO','PAYX','PYPL','PNR','PBCT','PEP','PKI','PRGO','PFE',
+'PCG','PM','PSX','PNW','PXD','PBI','PNC','RL','PPG','PPL','PX','PCLN','PFG','PG','PGR','PLD','PRU','PEG','PSA','PHM','PVH','QRVO',
+'QCOM','PWR','DGX','RRC','RTN','O','RHT','REGN','RF','RSG','RAI','RHI','ROK','COL','ROP','ROST','RCL','R','SPGI','CRM','SCG','SLB',
+'SNI','STX','SEE','SRE','SHW','SIG','SPG','SWKS','SLG','SJM','SNA','SO','LUV','SWN','SE','STJ','SWK','SPLS','SBUX','HOT','STT','SRCL',
+'SYK','STI','SYMC','SYF','SYY','TROW','TGT','TEL','TE','TGNA','TDC','TSO','TXN','TXT','BK','CLX','KO','HSY','MOS','TRV','DIS','TMO',
+'TIF','TWX','TJX','TMK','TSS','TSCO','TDG','RIG','TRIP','FOXA','FOX','TYC','TSN','USB','UDR','ULTA','UA','UNP','UAL','UNH','UPS',
+'URI','UTX','UHS','UNM','URBN','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','VIAB','V','VNO','VMC','WMT','WBA','WM','WAT','WFC',
+'HCN','WDC','WU','WRK','WY','WHR','WFM','WMB','WLTW','WEC','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YHOO','YUM','ZBH','ZION','ZTS']
 
 pool = ThreadPool(4)
 pool.map(bestGo, arr)
 pool.close()
 pool.join()
-        
-#submitArr = sortBest(bestCollect)
-        
-#os.remove("best.pickle")
+
+finalBest = []
+for val in bestCollect:
+    if ((val['future'][-1])/(val['stDev'][-1])) > 0.75:
+        finalBest.append(val)
+
+submitArr = sortBest(finalBest)
+
+os.remove("best.pickle")
 with open(r"best.pickle", "wb") as output_file:
     cPickle.dump(bestCollect, output_file)
-# In[ ]:
-
-
-
