2109044e41c0355f12ea86a58ab2edd465bc0035
Stephen Casella <sjcasella@icloud.com>
2016-06-23 13:48:12 -0400

    Update best.py

--- best.py
+++ best.py
@@ -73,10 +73,9 @@ def yahooLoad(val):
 
     csv = urllib2.urlopen(string).readlines()
     #for bar in xrange(1,min(len(csv),500)):
-    for bar in xrange(1,len(csv)):
+    for bar in xrange(1,min(50,len(csv))):
         close = csv[bar].split(',')[6]
-        close = float(close)
-        tempArr.append(close)
+        tempArr.append(float(close))
     tempArr = tempArr[::-1]
     return tempArr
 
